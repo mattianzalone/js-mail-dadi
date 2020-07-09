bf60a9c4b082ee6f302e7faf3c747fa3590e28c7
mattia <mattia24.studio@gmail.com>
2020-07-09 17:38:23 +0200

    added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-var email = prompt('Inserisci la tua email');
-var emailvalide = ['pippo@gmail.com', 'gennaro@gmail.com', 'mattia@virgilio.it', 'ciccio@gmail.com'];
-var trovata = false;
-for (var i = 0; i<emailvalide.length; i++){
-    if (email == emailvalide[i]){
-        trovata = true;
-    }
-}
-if (trovata == true){
-    alert('Email trovata');
+// Generare un numero random da 1  a 6, sia per il giocatore sia per il computer
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// variabili utente e computer
+var dadoutente = Math.floor(Math.random()*6)+1;
+var dadocomputer = Math.floor(Math.random()*6)+1;
+document.getElementById('utente').innerHTML = 'Questo è il dado dell\'utente' + ' ' + dadoutente;
+document.getElementById('computer').innerHTML = 'Questo è il dado del computer' + ' ' + dadocomputer;
+if (dadoutente > dadocomputer){
+    document.getElementById('vincitore').innerHTML = 'Il vincitore è l\'utente';
+} else if (dadocomputer > dadoutente) {
+    document.getElementById('vincitore').innerHTML = 'Il vincitore è il computer';
 } else {
-    alert('Email non presente');
+    document.getElementById('vincitore').innerHTML = 'I dadi sono uguali, non si può calcolare un vincitore';
 }
